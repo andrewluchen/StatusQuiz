8a0736bab71aca51e90ed2fc8837ce255ba171d1
Andrew Chen <andch43212@gmail.com>
2014-09-13 01:26:59 -0400

    adding randomization to names

--- static/app.js
+++ static/app.js
@@ -1,11 +1,17 @@
 Parse.initialize("ONeIYsbSCtIJg6bo0M823t6Z8ZqmqEM4Zfgh2U5a", "A1lJ2mF6YV48qcO5KBkQ1XCEC2BDF3mAt1MlqqlB");
 
-function updateText(postlist) {
-  message = postlist[0].message
-  person1 = postlist[0].name
-  person2 = postlist[1].name
-  person3 = postlist[2].name
-  person4 = postlist[3].name
+function updateText(postslist) {
+  var arr = [];
+  for(var i = 0; i < 4; i++) {
+    arr[i] = postslist[Math.floor((Math.random() * postslist.length))];
+  }
+  var chosen = Math.floor((Math.random() * postslist.length));
+  var message = postlist[chosen].message + " answer:" + postslist[chosen].name;
+  arr[Math.floor((Math.random() * 4))] = postslist[chosen]
+  var person1 = arr[0].name;
+  var person2 = arr[1].name;
+  var person3 = arr[2].name;
+  var person4 = arr[3].name;
   $('#statusPost').text(message);
   $('#pic1').text(person1);
   $('#pic2').text(person2);
