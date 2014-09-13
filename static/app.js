ce5219dd4a0eca30a433fe8a1db0285767312b2c
Justin Lee <justinlee1995@gmail.com>
2014-09-13 04:28:08 -0400

    Scoring system implemented

--- static/app.js
+++ static/app.js
@@ -2,7 +2,20 @@ Parse.initialize("ONeIYsbSCtIJg6bo0M823t6Z8ZqmqEM4Zfgh2U5a", "A1lJ2mF6YV48qcO5KB
 
 var prevPosts = [];
 var postslist = null;
+var chosen = 10;
+var alreadyChosen = false;
+var score = 0;
+
 function updateText() {
+  alreadyChosen = false;
+  $('#panel1').toggleClass('falsebox',false);
+  $('#panel1').toggleClass('truebox',false);
+  $('#panel2').toggleClass('falsebox',false);
+  $('#panel2').toggleClass('truebox',false);
+  $('#panel3').toggleClass('falsebox',false);
+  $('#panel3').toggleClass('truebox',false);
+  $('#panel4').toggleClass('falsebox',false);
+  $('#panel4').toggleClass('truebox',false);
   var arr = [];
   for(var i = 0; i < 4; i++) {
     var addpost = function(){
@@ -15,13 +28,13 @@ function updateText() {
     };
     addpost();
   }
-  var chosen = Math.floor((Math.random() * 4));
+  chosen = Math.floor((Math.random() * 4));
   for(var k = 0;k<prevPosts.length;k++){
       if(arr[chosen].postid===prevPosts[k])
 	chosen = Math.floor((Math.random() * 4));
   }
   prevPosts.push(chosen);
-  var message = arr[chosen].message + " answer:" + arr[chosen].name;
+  var message = arr[chosen].message;
   $('#statusPost').text(message);
   $('#img1').attr('src',"https://graph.facebook.com/" + arr[0].nameid + "/picture?type=large");
   $('#name1').text(arr[0].name);
@@ -31,7 +44,61 @@ function updateText() {
   $('#name3').text(arr[2].name);
   $('#img4').attr('src',"https://graph.facebook.com/" + arr[3].nameid + "/picture?type=large");
   $('#name4').text(arr[3].name);
+  $('#next').prop('disabled', true);
 }
+
+function correctAns(num){
+  if(!alreadyChosen){
+      if(num===chosen){
+	if(num===0){
+	  $('#panel1').toggleClass('truebox',true);
+	}
+	else if(num===1){
+	  $('#panel2').toggleClass('truebox',true);
+	}
+	else if(num===2){
+	  $('#panel3').toggleClass('truebox',true);
+	}
+	else if(num===3){
+	  $('#panel4').toggleClass('truebox',true);
+	}
+	score+=10;
+	
+      }
+      else if(num!==chosen){
+	if(num===0){
+	  $('#panel1').toggleClass('falsebox',true);
+	}
+	else if(num===1){
+	  $('#panel2').toggleClass('falsebox',true);
+	}
+	else if(num===2){
+	  $('#panel3').toggleClass('falsebox',true);
+	}
+	else if(num===3){
+	  $('#panel4').toggleClass('falsebox',true);
+	}
+	if(chosen===0){
+	  $('#panel1').toggleClass('truebox',true);
+	}
+	else if(chosen===1){
+	  $('#panel2').toggleClass('truebox',true);
+	}
+	else if(chosen===2){
+	  $('#panel3').toggleClass('truebox',true);
+	}
+	else if(chosen===3){
+	  $('#panel4').toggleClass('truebox',true);
+	}
+	score-=5;
+      }
+    }
+    $('#scorelabel').text(score);
+    $('#next').prop('disabled', false);
+    alreadyChosen = true;
+    chosen = 10;
+}
+
 
 function test(){
   var TestObject = Parse.Object.extend("TestObject");
