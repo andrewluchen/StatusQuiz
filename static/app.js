ac1b4ae30353485a27e79ada5c05a03fe888ec40
Andrew Chen <andch43212@gmail.com>
2014-09-13 01:12:25 -0400

    adding names

--- static/app.js
+++ static/app.js
@@ -1,7 +1,16 @@
 Parse.initialize("ONeIYsbSCtIJg6bo0M823t6Z8ZqmqEM4Zfgh2U5a", "A1lJ2mF6YV48qcO5KBkQ1XCEC2BDF3mAt1MlqqlB");
 
-function updateText(statuspost) {
-  $('#statusPost').text(statuspost);
+function updateText(postlist) {
+  message = postlist[0].message
+  person1 = postlist[0].name
+  person2 = postlist[1].name
+  person3 = postlist[2].name
+  person4 = postlist[3].name
+  $('#statusPost').text(message);
+  $('#pic1').text(person1);
+  $('#pic2').text(person2);
+  $('#pic3').text(person3);
+  $('#pic4').text(person4);
 }
 
 function test(){
