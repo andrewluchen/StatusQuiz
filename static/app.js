601b28ebc19dd19d256a094d60825844d8559229
Andrew Chen <andch43212@gmail.com>
2014-09-13 00:56:53 -0400

    adding testcode

--- static/app.js
+++ static/app.js
@@ -1,5 +1,9 @@
 Parse.initialize("ONeIYsbSCtIJg6bo0M823t6Z8ZqmqEM4Zfgh2U5a", "A1lJ2mF6YV48qcO5KBkQ1XCEC2BDF3mAt1MlqqlB");
 
+function insertMessage(statuspost) {
+  $('#statusPost').text(statuspost);
+}
+
 function test(){
   var TestObject = Parse.Object.extend("TestObject");
 
